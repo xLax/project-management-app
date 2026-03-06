f2b4e48eacfff7aed35a76d093657af1d389293b
Liron Levi <30602376+xLax@users.noreply.github.com>
2026-03-06 15:00:21 +0200

    Cors Error
    Fixed the cors error

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,6 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-app.options('*', cors());
 app.use(express.json());
 
 // Routes
